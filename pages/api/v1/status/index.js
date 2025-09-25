bc88a52848bc4c0c38afa7974177163c37e51cb6
José Augusto <joseaugusto0408@gmail.com>
2025-09-25 07:17:51 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database.js"
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 as rows;')
+  console.log(result.rows)
   response.status(200).json({
-    'chave': 'Alos do curso.dev'
+    'chave': 'Olá nós  no curso.dev'
   })
 }
 
