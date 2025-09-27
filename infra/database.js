f4de43189e2259b469a1120a27df042d27f58521
José Augusto <joseaugusto0408@gmail.com>
2025-09-27 14:45:44 -0300

    move `.env` file to `.env.development`

--- infra/database.js
+++ infra/database.js
@@ -2,10 +2,10 @@ import { Client } from 'pg';
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.POSTGRES_HOST,
+    host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
+    database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
